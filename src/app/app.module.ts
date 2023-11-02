bfcf3f4438c6142e8df65072865f960052710fdc
Ivirson <ivi_daltro@hotmail.com>
2023-11-01 21:57:49 -0300

    Aula 01 - criação das telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMaskPipe, provideEnvironmentNgxMask } from 'ngx-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { CreateUserComponent } from './modules/users/components/create-user/create-user.component';
 import { ListUsersComponent } from './modules/users/components/list-users/list-users.component';
 import { UsersComponent } from './modules/users/users.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -29,8 +31,10 @@ import { NotFoundComponent } from './core/components/not-found/not-found.compone
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
+    NgxMaskPipe,
   ],
-  providers: [],
+  providers: [provideEnvironmentNgxMask()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/users/components/list-users/list-users.component.ts
+++ src/app/modules/users/components/list-users/list-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-list-users',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.scss'],
 })
 export class ListUsersComponent implements OnInit {
+  public users: User[] = [];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.users.push({
+      name: 'Ivirson Daltro',
+      profession: 'Dev',
+      birthDate: '01/01/2000',
+      documentNumber: '01234567890',
+      email: 'ivirson@email.com',
+      password: 'SenhaForte@123',
+      phone: '71989898989',
+      income: 1000,
+      address: {
+        zipCode: '42800040',
+        street: 'Rua Costa Pinto',
+        number: 200,
+        neighborhood: 'Centro',
+        city: 'Camaçari',
+        state: 'BA',
+      },
+    });
+  }
 }
